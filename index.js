627dd98335b86c63a226695151c4df9c8da09c65
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-03 11:35:06 +0300

    lab.md

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(Broom) {;
+    return [...cats, "Broom"];
+}
+
+function prependCat(Arnold) {
+    return ["Arnold", ...cats]
+}
+function removeLastCat() {
+   return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
